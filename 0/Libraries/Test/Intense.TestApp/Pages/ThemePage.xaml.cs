992822cde814df550495f85fe4aea46c09fe9789
Koen Zwikstra <kozw@firstfloorsoftware.com>
2016-03-05 16:54:10 +0100

    test app command demo pages updated

--- 0/Libraries/Test/Intense.TestApp/Pages/ThemePage.xaml.cs
+++ 0/Libraries/Test/Intense.TestApp/Pages/ThemePage.xaml.cs
@@ -21,9 +21,9 @@ namespace Intense.TestApp.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class AppearancePage : Page
+    public sealed partial class ThemePage : Page
     {
-        public AppearancePage()
+        public ThemePage()
         {
             this.InitializeComponent();
         }
