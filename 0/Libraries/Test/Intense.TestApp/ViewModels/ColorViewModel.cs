39946509f0cc50c4f04e4e79263dc669c730172b
Koen Zwikstra <kozw@firstfloorsoftware.com>
2016-03-24 17:32:42 +0100

    added color and theme appearance pages to test app

---
+++ 0/Libraries/Test/Intense.TestApp/ViewModels/ColorViewModel.cs
@@ -0,0 +1,88 @@
+﻿using Intense.Presentation;
+using Intense.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+
+namespace Intense.TestApp.ViewModels
+{
+    /// <summary>
+    /// The view model for managing the accent color.
+    /// </summary>
+    public class ColorViewModel
+        : NotifyPropertyChanged, IAppearanceManagerEventSink
+    {
+        private SolidColorBrush selectedBrush;
+        private bool useSystemAccentColor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorViewModel"/>.
+        /// </summary>
+        public ColorViewModel()
+        {
+            this.Brushes = AccentColors.Windows10.Select(c => new SolidColorBrush(c)).ToImmutableList();
+
+            SyncSelectedBrush();
+        }
+
+        private void SyncSelectedBrush()
+        {
+            if (this.UseSystemAccentColor = !AppearanceManager.AccentColor.HasValue) {
+                this.SelectedBrush = null;
+            }
+            else {
+                this.SelectedBrush = this.Brushes.FirstOrDefault(b => b.Color == AppearanceManager.AccentColor);
+            }
+        }
+
+        void IAppearanceManagerEventSink.OnAccentColorChanged(object source, EventArgs e)
+        {
+            SyncSelectedBrush();
+        }
+
+        void IAppearanceManagerEventSink.OnSystemAccentColorChanged(object source, EventArgs e)
+        {
+        }
+
+        void IAppearanceManagerEventSink.OnThemeChanged(object source, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Gets the brushes.
+        /// </summary>
+        public IReadOnlyList<SolidColorBrush> Brushes { get; }
+
+        /// <summary>
+        /// Gets or sets the selected brush.
+        /// </summary>
+        public SolidColorBrush SelectedBrush
+        {
+            get { return this.selectedBrush; }
+            set
+            {
+                if (Set(ref this.selectedBrush, value)) {
+                    AppearanceManager.AccentColor = this.useSystemAccentColor ? null : value?.Color;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to use the system accent color.
+        /// </summary>
+        public bool UseSystemAccentColor
+        {
+            get { return this.useSystemAccentColor; }
+            set
+            {
+                if (Set(ref this.useSystemAccentColor, value)) {
+                    AppearanceManager.AccentColor = value ? null : this.SelectedBrush?.Color;
+                }
+            }
+        }
+    }
+}
---
+++ 0/Libraries/Test/Intense.TestApp/ViewModels/ThemeViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Intense.Presentation;
+using Intense.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Intense.TestApp.ViewModels
+{
+    /// <summary>
+    /// The view model for managing the theme.
+    /// </summary>
+    public class ThemeViewModel
+        : NotifyPropertyChanged, IAppearanceManagerEventSink
+    {
+        private DisplayableTheme selectedTheme;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThemeViewModel"/>.
+        /// </summary>
+        public ThemeViewModel()
+        {
+            this.Themes = ImmutableList.Create(
+                new DisplayableTheme("Dark", ApplicationTheme.Dark),
+                new DisplayableTheme("Light", ApplicationTheme.Light));
+
+            SyncSelectedTheme();
+        }
+
+        private void SyncSelectedTheme()
+        {
+            var manager = AppearanceManager.GetForCurrentView();
+            this.SelectedTheme = this.Themes.FirstOrDefault(t => t.Theme == manager.Theme);
+        }
+
+        void IAppearanceManagerEventSink.OnAccentColorChanged(object source, EventArgs e)
+        {
+        }
+
+        void IAppearanceManagerEventSink.OnSystemAccentColorChanged(object source, EventArgs e)
+        {
+        }
+
+        void IAppearanceManagerEventSink.OnThemeChanged(object source, EventArgs e)
+        {
+            SyncSelectedTheme();
+        }
+
+        /// <summary>
+        /// Gets the collection of themes.
+        /// </summary>
+        public IReadOnlyList<DisplayableTheme> Themes { get; }
+        /// <summary>
+        /// Gets or sets the selected theme.
+        /// </summary>
+        public DisplayableTheme SelectedTheme
+        {
+            get { return this.selectedTheme; }
+            set
+            {
+                if (value != null && Set(ref this.selectedTheme, value)) {
+                    AppearanceManager.GetForCurrentView().Theme = value.Theme;
+                }
+            }
+        }
+    }
+}
