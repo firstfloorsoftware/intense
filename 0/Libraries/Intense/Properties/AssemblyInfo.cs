80564d238167e3a99416dd1381bee82e8e2234c9
Koen Zwikstra <kozw@firstfloorsoftware.com>
2016-03-26 12:34:25 +0100

    version set to 0.5.0

--- 0/Libraries/Intense/Properties/AssemblyInfo.cs
+++ 0/Libraries/Intense/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.4.0")]
-[assembly: AssemblyFileVersion("0.4.0")]
-[assembly: AssemblyInformationalVersion("0.4.0")]
+[assembly: AssemblyVersion("0.5.0")]
+[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyInformationalVersion("0.5.0")]
 [assembly: ComVisible(false)]
